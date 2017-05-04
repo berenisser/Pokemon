50dabc572fa329fccb2b5dad7bcdd4846d41264e
berenisser <berenisser@gmail.com>
2017-05-03 22:49:40 -0300

    nivelDeAmistad creada

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,6 +1,11 @@
 function Pokemon(nombre,color){
 	this.nombre = nombre;
 	this.color = color;
+
+	this.nivelDeAmistad = 0;
+	this.mostrarPokemon = function(){
+		return("Hola, soy: " + this.nombre + " y soy de color: " + this.color);
+	}
 }
 
 const Pikachu = new Pokemon("Pikachu","amarillo");
